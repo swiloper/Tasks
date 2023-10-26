cfb3552848d87f70e98f9ad0e9b51e3718ca0871
swiloper <myronishyn.ihor@icloud.com>
2023-10-26 16:44:54 +0300

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Ihor Myronishyn on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
